e6cb9620479b6e47bea99d300228145e3426e27f
augusto <augusdpmenezes@gmail.com>
2023-06-26 13:25:05 -0300

    início

--- all-projects/src/App.jsx
+++ all-projects/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const App = () => {
+  const projects = [
+    {
+      title: 'AD Houses',
+    }
+  ]
   return (
-    <div>App</div>
+    <>
+      <main className='min-h-screen p-20'>
+        <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5'>
+          {projects.map((project) => (
+            <div className='border text-offwhite p-4 border-tertiary rounded-md bg-tertiary/30 h-[300px] w-full hover:shadow-md hover:shadow-tertiary transition duration-300 cursor-pointer'>
+              <div className='flex flex-col justify-between h-full'>
+                <p className='font-title font-bold text-center text-4xl mt-24'>{project.title}</p>
+                <div className='flex flex-row w-full gap-2'>
+                <a href="" className='w-full bg-tertiary/30 text-center border border-tertiary p-1 rounded-md hover:bg-tertiary transition duration-200'>Demo</a>
+                <a href="" className='w-full bg-tertiary/30 text-center border border-tertiary p-1 rounded-md hover:bg-tertiary transition duration-200'>Código</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
   )
 }
 
--- all-projects/tailwind.config.js
+++ all-projects/tailwind.config.js
@@ -4,8 +4,23 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['Oswald', 'sans-serif']
+      },
+      colors: {
+        primary: '#060e33',
+        secondary: '#152372',
+        // tertiary: '#5281C6',
+        tertiary: '#1E4976',
+        quartiary: '#e6e6ff',
+        textColor: '#f1f1f1',
+        navColor: '#E2E1E4',
+        offwhite: '#f1f1f1',
+      }
+    },
   },
   plugins: [],
 }
